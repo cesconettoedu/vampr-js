096a04f81441b6e2b2efa7d3a4cf7d81942694c9
duducesconetto <cesconettoedu@gmail.com>
2022-08-07 18:59:39 +0000

    addOffsping and numberOfoffspirng ok

--- vampire.js
+++ vampire.js
@@ -8,14 +8,16 @@ class Vampire {
 
   /** Simple tree methods **/
 
-  // Adds the vampire as an offspring of this vampire
+  // Adds the vampire as an offspring of this vampire      OKKKK
   addOffspring(vampire) {
+    this.offspring.push(vampire);
+    vampire.creator = this;
 
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
